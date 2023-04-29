c4fbcd9dcc78b0ffea542eb40bf60fc1182fbbec
disskretkalove <96077142+disskretkalove@users.noreply.github.com>
2023-04-29 09:56:49 +0300

    Add files via upload

---
+++ LR_2/zadacha_4/student_list_strategy.rb
@@ -0,0 +1,10 @@
+class StudentListStrategy
+  private_class_method :new
+
+  protected
+
+  # Шаблон паттерн
+  def str_to_list(str); end
+
+  def list_to_str(list); end
+end
---
+++ LR_2/zadacha_4/student_list_yaml.rb
@@ -0,0 +1,16 @@
+require_relative 'student_list_strategy'
+require 'yaml'
+
+class StudentListYAML < StudentListStrategy
+  public_class_method :new
+
+  protected
+
+  def str_to_list(str)
+    YAML.safe_load(str).map { |h| h.transform_keys(&:to_sym) }
+  end
+
+  def list_to_str(list)
+    YAML.dump(list.map { |h| h.transform_keys(&:to_s) })
+  end
+end
